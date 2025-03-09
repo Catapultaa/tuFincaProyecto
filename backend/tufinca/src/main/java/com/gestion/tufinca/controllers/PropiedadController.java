112c43a2d06a8002410956afd6c6ea135afa3d15
Millimillan1 <camimillan2003@gmail.com>
2025-03-08 23:33:11 -0500

    prueba inicial y desactivacion temporal de spring security

--- backend/tufinca/src/main/java/com/gestion/tufinca/controllers/PropiedadController.java
+++ backend/tufinca/src/main/java/com/gestion/tufinca/controllers/PropiedadController.java
@@ -1,4 +1,15 @@
 package com.gestion.tufinca.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/propiedades")
 public class PropiedadController {
+
+    @GetMapping("/")
+    public String holaPropiedades(){
+        return "api de propiedades";
+    }
 }
