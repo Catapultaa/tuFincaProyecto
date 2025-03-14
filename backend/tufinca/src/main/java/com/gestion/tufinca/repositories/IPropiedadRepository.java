dbe23854ba13bce53aeff0ae390011ac620b3db2
Millimillan1 <camimillan2003@gmail.com>
2025-03-13 21:06:18 -0500

    .\Agregando ejemplos de toda la estructura de los modelos y repositoriosv.2

--- backend/tufinca/src/main/java/com/gestion/tufinca/repositories/IPropiedadRepository.java
+++ backend/tufinca/src/main/java/com/gestion/tufinca/repositories/IPropiedadRepository.java
@@ -1,6 +1,6 @@
 package com.gestion.tufinca.repositories;
 
-import com.gestion.tufinca.models.PropiedadModel
+import com.gestion.tufinca.models.PropiedadModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
